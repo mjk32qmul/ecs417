75526d59dfc9d943b2b0a62bf875d05578dccfc2
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-01 21:44:13 +0100

    Add files via upload

---
+++ webroot/Blog.php
@@ -0,0 +1,15 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>My Site</title>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php
+            echo "name = " .$_SESSION["username"];
+            echo "password = " .$_SESSION["password"];
+        ?>
+    </body>
+</html>
---
+++ webroot/Login.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+?>
+
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>My Site</title>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php
+            $_SESSION["username"] = "username";
+            $_SESSION["password"] = "password";
+            echo "name = " .$_SESSION["username"];
+            echo "password = " .$_SESSION["password"];
+        ?>
+    </body>
+</html>
