80a570db9ba7b5b3bed832a666ff5da0696f3e3b
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-01 22:55:42 +0100

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+?>
+
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>My Site</title>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php
+            $_SESSION["username"] = "username";
+            $_SESSION["password"] = "password";
+            echo "name = " .$_SESSION["username"];
+            echo "password = " .$_SESSION["password"];
+
+            $dbhost = getenv("127.0.0.1");
+            $dbuser = getenv("admin");
+            $dbpwd = getenv("password");
+            $dbname = getenv("ecs417");
+            // Creates connection
+            $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+            // Checks connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+        ?>
+    </body>
+</html>
