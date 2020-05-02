055c2181e9da74af12cd6ff92c35ab8438a27179
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-02 03:15:17 +0100

    Add files via upload

---
+++ webroot/Login.php
@@ -0,0 +1,65 @@
+<?php
+    session_start();
+?>
+
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>My Site</title>
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <?php
+            $dbhost = getenv("MYSQL_SERVICE_HOST");
+            $dbport = getenv("MYSQL_SERVICE_PORT");
+            $dbuser = getenv("DATABASE_USER");
+            $dbpwd = getenv("DATABASE_PASSWORD");
+            $dbname = getenv("DATABASE_NAME"); 
+            // Creates connection
+            $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+            // Checks connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            else{
+                echo "Connection sucessful\n<br>";
+            }
+
+            $password = "Bananas Ar3 V3ry tasty!";
+            $username = "Matthew King";
+            
+            $sql = "SELECT username, password, firstname, lastname, email FROM USERS WHERE username=".$username."password=".crypt($password, '$5$anexamplestringforsalt$');
+            echo $sql;
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    echo "id: " . $row["username"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. " - Email " . $row["email"]. " - Password " . $row["password"]."<br>";
+                }
+            } else {
+                echo "0 results";
+            }
+            $conn->close();
+
+            // 2 character salt
+
+            // 16 character salt starting with $5$. The default number of rounds is 5000.
+            if (CRYPT_SHA256 == 1)
+            {
+            echo "SHA-256: ".crypt('Bananas Ar3 V3ry tasty!','$5$anexamplestringforsalt$')."\n<br>"; }
+            else
+            {
+            echo "SHA-256 not supported.\n<br>";
+            }
+
+
+            /*$_SESSION["username"] = "username";
+            $_SESSION["password"] = "password";
+            echo "name = " .$_SESSION["username"];
+            echo "password = " .$_SESSION["password"];*/
+        ?>
+    </body>
+</html>
