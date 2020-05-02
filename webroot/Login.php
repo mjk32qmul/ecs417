5b37784f6805bd886a8411832a96a4f39f0d6fc5
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-02 04:36:28 +0100

    Add files via upload

--- webroot/Login.php
+++ webroot/Login.php
@@ -2,6 +2,53 @@
     session_start();
 ?>
 
+<?php
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbport = getenv("MYSQL_SERVICE_PORT");
+    $dbuser = getenv("DATABASE_USER");
+    $dbpwd = getenv("DATABASE_PASSWORD");
+    $dbname = getenv("DATABASE_NAME"); 
+    // Creates connection
+    $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+    // Checks connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else{
+        echo "Connection sucessful\n<br>";
+    }
+
+    $password = "Bananas Ar3 V3ry tasty!";
+    $username = "Matthew-King";
+
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+    
+    $sql = "SELECT ID, username, password, firstname, lastname, email FROM USERS WHERE
+    username='".$username."'";
+    $sha256pass = crypt($password, '$5$anexamplestringforsalt$');
+    echo $sql. "\n<br>";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            if ($row["password"] == $sha256pass){
+                echo "You're logged in as ".$row["username"];
+                $_SESSION["ID"] = $row["ID"];
+                $_SESSION["username"] = $row["username"];
+                $_SESSION["firstname"] = $row["firstname"];
+                $_SESSION["lastname"] = $row["lastname"];
+                $_SESSION["email"] = $row["email"];
+                $_SESSION["password"] = $row["password"];
+                $_SESSION["loggedIn"] = true;
+                header("Location: Blog.php");
+            }
+        }
+    } else {echo "sorry but the wasn't correct";}
+    $conn->close();
+?>
+
 <!Doctype html>
 <html>
     <head>
@@ -11,51 +58,5 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
     <body>
-        <?php
-            $dbhost = getenv("MYSQL_SERVICE_HOST");
-            $dbport = getenv("MYSQL_SERVICE_PORT");
-            $dbuser = getenv("DATABASE_USER");
-            $dbpwd = getenv("DATABASE_PASSWORD");
-            $dbname = getenv("DATABASE_NAME"); 
-            // Creates connection
-            $conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-            // Checks connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-            else{
-                echo "Connection sucessful\n<br>";
-            }
-
-            $password = "Bananas Ar3 V3ry tasty!";
-            $username = "Matthew-King";
-
-            $password = $_POST["password"];
-            $username = $_POST["username"];
-            
-            $sql = "SELECT ID, username, password, firstname, lastname, email FROM USERS WHERE
-            username='".$username."'";
-            $sha256pass = crypt($password, '$5$anexamplestringforsalt$');
-            echo $sql. "\n<br>";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while($row = $result->fetch_assoc()) {
-                    if ($row["password"] == $sha256pass){
-                        echo "You're logged in as ".$row["username"];
-                        $_SESSION["ID"] = $row["ID"];
-                        $_SESSION["username"] = $row["username"];
-                        $_SESSION["firstname"] = $row["firstname"];
-                        $_SESSION["lastname"] = $row["lastname"];
-                        $_SESSION["email"] = $row["email"];
-                        $_SESSION["password"] = $row["password"];
-                        $_SESSION["loggedIn"] = true;
-                        header("Blog.php");
-                    }
-                }
-            } else {echo "sorry but the wasn't correct";}
-            $conn->close();
-        ?>
     </body>
 </html>
