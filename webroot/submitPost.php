c42704bad7a454513354288cf91f44ee98c20217
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-16 23:06:32 +0100

    Add files via upload

--- webroot/submitPost.php
+++ webroot/submitPost.php
@@ -33,8 +33,8 @@
     <head>
         <meta charset="UTF-8" />
         <title>My Site</title>
-        <link rel="stylesheet" type="text/css" href="css/reset.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="webroot/css/reset.css">
+        <link rel="stylesheet" type="text/css" href="weebroot/css/style.css">
     </head>
     <body>
     </body>
