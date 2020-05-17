6d392cbd494b86acf7337b5b1d479955ea456675
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-17 14:50:24 +0100

    Add files via upload

--- webroot/Blog.php
+++ webroot/Blog.php
@@ -1,5 +1,26 @@
 <?php
-    session_start();
+
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	//$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("DATABASE_USER");
+	$dbpwd = getenv("DATABASE_PASSWORD");
+	$dbname = getenv("DATABASE_NAME"); 
+	// Creates connection
+	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	// Checks connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	$sqlQuery = "SELECT * FROM POSTS ORDER BY ID DESC";
+	$result = $conn->query($sql);
+	$entries = array();
+
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			array_push($entries, array($result["time"], $result["username"], $result["contents"]));
+		}
+	}
+	$conn->close();
 ?>
 
 <!Doctype html>
@@ -7,20 +28,21 @@
     <head>
         <meta charset="UTF-8" />
         <title>My Site</title>
-        <link rel="stylesheet" type="text/css" href="css/reset.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="webroot/css/reset.css">
+        <link rel="stylesheet" type="text/css" href="webroot/css/style.css">
     </head>
     <body>
-        <?php
-            echo "name = " .$_SESSION["username"]."\n<br>";
-            echo "password = " .$_SESSION["password"]."\n<br>";
-            echo "id = " .$_SESSION["ID"]."\n<br>";
-            echo "firstname = " .$_SESSION["firstname"]."\n<br>";
-            echo "lastname = " .$_SESSION["lastname"]."\n<br>";
-            echo "email = " .$_SESSION["email"]."\n<br>";
-            if ($_SESSION["loggedIn"]){
-                echo "You have been logged in \n<br>";
-            }
-        ?>
+		<article>
+			<?php
+				for ($row = 0; $row < count($entries); $row++){
+					echo "<section id='post'>";
+					echo "<p id='username'>".$entries[$row][1]."</p>";
+					echo "<p id='time'>".$entries[$row][0]."</p>";
+					echo "<br/>";
+					echo "<p id='contents'>".$entries[$row][2]."</p>";
+					echo "</section>";
+				}
+			?>
+		</article>
     </body>
 </html>
