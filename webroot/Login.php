212f668d6d23115023a5cc2dc885a0676afab5e0
mjk32qmul <60140270+mjk32qmul@users.noreply.github.com>
2020-05-20 01:20:37 +0100

    Add files via upload

--- webroot/Login.php
+++ webroot/Login.php
@@ -23,7 +23,7 @@
 		$password = $_POST["password"];
 		$emial = $_POST["email"];
 		$sha256pass = crypt($password, '$5$anexamplestringforsalt$');
-		echo "Email:".$email;
+		echo "Email:".$email.":".$_POST["email"];
 		$sql = "SELECT * FROM USERS WHERE
 		email='".$email."'"."AND password = '".$sha256pass."'";
 		$result = $conn->query($sql);
